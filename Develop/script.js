a102cb33a1854337d0abbd1194707cb0222635b7
Raghad Malallah <raghadmalallah@gmail.com>
2022-08-19 09:06:01 -0500

    Refactoring the Site

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,12 @@ var generateBtn = document.getElementById("#generate");
 
 var onlyUppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var onlyLowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var onlySpecialcase = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+var onlySpecialcase = ['#','$','%','&','*','+',',','-','/',':',';','<','=','>','?','@','['];
 var onlyNumeric = [0,1,2,3,4,5,6,7,8,9];
 
 
 function generateOptions() {
-  var length = parseInt(prompt("How many charecters do you want your password to be?"));
+  var length = parseInt(prompt("How many characters do you want your password to be?"));
 
   if (length < 8) {
   alert('The password must be at least 8 characters!');
